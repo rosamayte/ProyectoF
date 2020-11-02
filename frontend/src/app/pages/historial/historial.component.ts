598ecf972010176579241b8fea5ea6b289668c9c
Daniel Mendoza <arceus6666@hotmail.com>
2020-11-02 18:24:47 -0400

    historial

--- frontend/src/app/pages/historial/historial.component.ts
+++ frontend/src/app/pages/historial/historial.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistorialComponent implements OnInit {
 
+  private dataOriginal = null;
+  public busqueda = '';
+  public dataVisual = null;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.dataOriginal = [
+      {
+        fecha: new Date(),
+        nombre: "Lisa Smith",
+        precio: 2,
+        cantidad: 12,
+        cv: "c"
+      },
+      {
+        fecha: new Date(),
+        nombre: "misa Smith",
+        precio: 2,
+        cantidad: 12,
+        cv: "c"
+      }, {
+        fecha: new Date(),
+        nombre: "naruto Smith",
+        precio: 2,
+        cantidad: 12,
+        cv: "c"
+      }
+    ];
+    this.dataVisual = [...this.dataOriginal];
+  }
+
+  public searchByName(dataO = this.dataOriginal) {
+    if (this.busqueda.length === 0) {
+      this.dataVisual = [...dataO];
+      return;
+    }
+    if (!this.busqueda) return;
+    this.dataVisual = dataO.filter(d => d.nombre.toLowerCase().includes(this.busqueda.toLowerCase()));
   }
 
 }
--- frontend/src/app/pages/historial/historial.module.ts
+++ frontend/src/app/pages/historial/historial.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {HistorialComponent} from './historial.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '**', component: HistorialComponent}
@@ -14,6 +15,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class HistorialModule { }
