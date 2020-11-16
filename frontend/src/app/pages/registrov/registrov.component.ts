b990bc3a6fe097ca560ddf71e62c6b86d886fc8f
Daniel Mendoza <arceus6666@hotmail.com>
2020-11-16 17:05:04 -0400

    registrov

--- frontend/src/app/pages/registrov/registrov.component.ts
+++ frontend/src/app/pages/registrov/registrov.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrovComponent implements OnInit {
 
+  itemList = ["i1",'i2','i3'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addItem(item){
   }
 
 }
